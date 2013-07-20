862ac20d3cbe3534d799803567166e1baf7c1498
Helder <he7d3r@gmail.com>
2013-07-20 14:35:37 -0300

    Move from common.js to vector.js, and from JSLint to JSHint

--- SyncMyTools.js
+++ SyncMyTools.js
@@ -3,17 +3,17 @@
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/SyncMyTools.js]] ([[File:User:Helder.wiki/Tools/SyncMyTools.js]])
  */
-/*jslint browser: true, white: true*/
-/*global mediaWiki, jQuery*/
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
 ( function ( mw, $ ) {
 'use strict';
 
-function syncCommonJS (){
-	var commonJsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/common.js';
-	$( '#firstHeading' ).injectSpinner( 'spinner-sync-common-js' );
+function syncJS (){
+	var JsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/vector.js';
+	$( '#firstHeading' ).injectSpinner( 'spinner-sync-js' );
 	( new mw.Api() ).post( {
 		action: 'edit',
-		title: commonJsPage,
+		title: JsPage,
 		text: '//{ {subst:User:Helder.wiki/Tools.js}}\n{' +
 			'{subst:User:Helder.wiki/Tools.js}}',
 		summary: 'Atualização a partir de [[User:Helder.wiki/Tools.js|/Tools.js]]',
@@ -24,15 +24,15 @@ function syncCommonJS (){
 	.done( function( data ) {
 		if ( data && data.edit && data.edit.result && data.edit.result === 'Success' ) {
 			mw.notify(
-				$( '<p>Seu common.js <a href="' +
-					mw.util.wikiGetlink( commonJsPage ) + '?diff=0' +
+				$( '<p>Seu vector.js <a href="' +
+					mw.util.wikiGetlink( JsPage ) + '?diff=0' +
 				'">foi editado</a>.</p>' )
 			);
 		} else {
-			mw.notify( 'Houve um erro ao tentar editar seu common.js' );
+			mw.notify( 'Houve um erro ao tentar editar seu vector.js' );
 		}
 	} ).always( function(){
-		$.removeSpinner( 'spinner-sync-common-js' );
+		$.removeSpinner( 'spinner-sync-js' );
 	} );
 }
 
@@ -40,17 +40,17 @@ function addSyncLink (){
 	$( mw.util.addPortletLink(
 		'p-cactions',
 		'#',
-		'Sincronizar common.js',
-		'ca-sync-common-js',
-		'Sincronizar o common.js com a versão mais recente dos seus scripts'
+		'Sincronizar vector.js',
+		'ca-sync-js',
+		'Sincronizar o vector.js com a versão mais recente dos seus scripts'
 	) ).click( function(e){
 		e.preventDefault();
-		mw.loader.using( [ 'mediawiki.api.edit', 'jquery.spinner' ], syncCommonJS );
+		mw.loader.using( [ 'mediawiki.api.edit', 'jquery.spinner' ], syncJS );
 	} );
 }
 
 if( /\.js$/.test( mw.config.get( 'wgTitle' ) ) && mw.config.get( 'wgDBname' ) === 'ptwikibooks' ){
-	$(addSyncLink);
+	$( addSyncLink );
 }
 
 }( mediaWiki, jQuery ) );

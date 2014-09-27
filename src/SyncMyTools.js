31a8fad637b3685269935c2b4642583cb41f2697
Helder <he7d3r@gmail.com>
2014-09-27 20:49:02 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/SyncMyTools.js
+++ src/SyncMyTools.js
@@ -4,53 +4,53 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function syncJS (){
-	var jsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/vector.js',
-		toolsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/Tools.js';
-	$( '#firstHeading' ).injectSpinner( 'spinner-sync-js' );
-	( new mw.Api() ).post( {
-		action: 'edit',
-		title: jsPage,
-		text: '//{ {subst:' + toolsPage + '}}\n{' +
-			'{subst:' + toolsPage + '}}',
-		summary: 'Atualização com {' + '{subst:[[' + toolsPage + ']]}}',
-		minor: true,
-		watchlist: 'nochange',
-		token: mw.user.tokens.get( 'editToken' )
-	} )
-	.done( function( data ) {
-		var edit = data.edit;
-		if ( edit && edit.result && edit.result === 'Success' ) {
-			mw.notify(
-				$( '<p>Seu vector.js <a href="' +
-					mw.util.getUrl( jsPage ) + '?diff=' + ( edit.newrevid || 0 ) +
-				'">foi editado</a>.</p>' )
-			);
-		} else {
-			mw.notify( 'Houve um erro ao tentar editar seu vector.js' );
-		}
-	} ).always( function(){
-		$.removeSpinner( 'spinner-sync-js' );
-	} );
-}
+	function syncJS() {
+		var jsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/vector.js',
+			toolsPage = 'User:' + mw.config.get( 'wgUserName' ) + '/Tools.js';
+		$( '#firstHeading' ).injectSpinner( 'spinner-sync-js' );
+		( new mw.Api() ).post( {
+			action: 'edit',
+			title: jsPage,
+			text: '//{ {subst:' + toolsPage + '}}\n{' +
+				'{subst:' + toolsPage + '}}',
+			summary: 'Atualização com {' + '{subst:[[' + toolsPage + ']]}}',
+			minor: true,
+			watchlist: 'nochange',
+			token: mw.user.tokens.get( 'editToken' )
+		} )
+		.done( function ( data ) {
+			var edit = data.edit;
+			if ( edit && edit.result && edit.result === 'Success' ) {
+				mw.notify(
+					$( '<p>Seu vector.js <a href="' +
+						mw.util.getUrl( jsPage ) + '?diff=' + ( edit.newrevid || 0 ) +
+					'">foi editado</a>.</p>' )
+				);
+			} else {
+				mw.notify( 'Houve um erro ao tentar editar seu vector.js' );
+			}
+		} ).always( function () {
+			$.removeSpinner( 'spinner-sync-js' );
+		} );
+	}
 
-function addSyncLink (){
-	$( mw.util.addPortletLink(
-		'p-cactions',
-		'#',
-		'Sincronizar vector.js',
-		'ca-sync-js',
-		'Sincronizar o vector.js com a versão mais recente dos seus scripts'
-	) ).click( function( e ){
-		e.preventDefault();
-		mw.loader.using( [ 'mediawiki.api.edit', 'jquery.spinner', 'mediawiki.notify' ], syncJS );
-	} );
-}
+	function addSyncLink() {
+		$( mw.util.addPortletLink(
+			'p-cactions',
+			'#',
+			'Sincronizar vector.js',
+			'ca-sync-js',
+			'Sincronizar o vector.js com a versão mais recente dos seus scripts'
+		) ).click( function ( e ) {
+			e.preventDefault();
+			mw.loader.using( [ 'mediawiki.api.edit', 'jquery.spinner', 'mediawiki.notify' ], syncJS );
+		} );
+	}
 
-if( /\.js$/.test( mw.config.get( 'wgTitle' ) ) && mw.config.get( 'wgDBname' ) === 'ptwikibooks' ){
-	$( addSyncLink );
-}
+	if ( /\.js$/.test( mw.config.get( 'wgTitle' ) ) && mw.config.get( 'wgDBname' ) === 'ptwikibooks' ) {
+		$( addSyncLink );
+	}
 
 }( mediaWiki, jQuery ) );
